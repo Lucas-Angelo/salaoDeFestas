c08b3b60b3b44c7bdc8d51ba847eef09c1021b52
Henrique Monteiro <henrique.mont011@gmail.com>
2020-06-13 19:45:54 -0300

    Add Contrato Class

---
+++ ContratoClass.h
@@ -0,0 +1,18 @@
+#ifndef CONTRATOCLASS_H
+#define CONTRATOCLASS_H
+
+
+class ContratoClass
+{
+    public:
+        ContratoClass();
+        int codigo;
+        float valor_total;
+        float desconto;
+        float valor_final;
+        char formaPagamento;
+        char status;
+        int codigo_festa;
+};
+
+#endif // CONTRATOCLASS_H
