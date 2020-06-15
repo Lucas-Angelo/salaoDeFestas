6a8076ff6ec49ca67e6f046993937a8ba07386f2
Lucas-Angelo <lcs2001_@hotmail.com>
2020-06-14 21:50:14 -0300

    fix

--- FuncionariosClass.h
+++ FuncionariosClass.h
@@ -13,7 +13,7 @@ class FuncionariosClass
         string telefone;
         string funcao;
         float salario;
-        char tipo;
+        string tipo;
 };
 
 #endif // FUNCIONARIOSCLASS_H
--- funcaoFuncionarios.cpp
+++ funcaoFuncionarios.cpp
@@ -14,44 +14,49 @@ string salvarNome();
 string salvarTelefone();
 string salvarFuncao();
 float salvarSalario();
-char salvarTipo();
+string salvarTipo();
 
 
 int funcaoFuncionarios()
 {
+    char confirmar;
     setlocale(LC_ALL,"portuguese"); //
     FuncionariosClass funcionarios;
 
-    cout << ">>> CADASTRAR FUNCIONÁRIO <<<" << endl;
+    cout << "\n>>> CADASTRAR FUNCIONÁRIO <<<" << endl;
 
-    funcionarios.codigo = gerarCodigo(); //Capturar código aleatório gerado.
-
-    funcionarios.nome = salvarNome(); //Receber nome digitado.
-    cout << funcionarios.nome << endl;
-
-    funcionarios.telefone = salvarTelefone(); //Receber telefone digitado.
-    cout << funcionarios.telefone << endl;
-
-    funcionarios.funcao = salvarFuncao(); //Receber função do funcionário digitada.
-    cout << funcionarios.funcao << endl;
-
-    funcionarios.salario = salvarSalario(); //Receber salário digitado.
-    cout << funcionarios.salario << endl;
-
-
-    funcionarios.tipo = salvarTipo(); //Receber tipo digitado.
-    switch (funcionarios.tipo)
+    do
     {
-        case 'T':
-            cout << "Temporário\n" << endl;
-            break;
-        case 'F':
-            cout << "Fixo\n" << endl;
-            break;
-        default:
-            cout << "Erro, por favor tente novamente.\n" << endl;
-            break;
-    }
+
+        funcionarios.codigo = gerarCodigo(); //Capturar código aleatório gerado.
+
+        funcionarios.nome = salvarNome(); //Receber nome digitado.
+
+        funcionarios.telefone = salvarTelefone(); //Receber telefone digitado.
+
+        funcionarios.funcao = salvarFuncao(); //Receber função do funcionário digitada.
+
+        funcionarios.salario = salvarSalario(); //Receber salário digitado.
+
+        funcionarios.tipo = salvarTipo(); //Receber tipo digitado.
+
+
+        cout << "\nDados do funcionário:" << endl;
+        printf("Código: %d\n", funcionarios.codigo);
+        cout << "Nome: " + funcionarios.nome << endl;
+        cout << "Telefone: " + funcionarios.telefone << endl;
+        cout << "Função: " + funcionarios.funcao << endl;
+        printf("Salário: %.2f\n", funcionarios.salario);
+        cout << "Tipo: " + funcionarios.tipo + '\n'  << endl;
+
+        printf("---> Confirmar dados <---");
+        printf("\nC - Para confirmar");
+        printf("\nR - Para refazer");
+        printf("\nOs dados inseridos estão corretos? ");
+        cin >> confirmar;
+    } while (toupper(confirmar)!='C');
+
+    cout << "\n---> FUNCIONÁRIO CADASTRADO COM SUCESSO <---\n\n" << endl;
 
 
 
@@ -61,24 +66,41 @@ int funcaoFuncionarios()
 unsigned int gerarCodigo() //Função para gerar o código aleatório
 {
     srand((unsigned int)time(NULL)); //Só deve ser chamada uma única vez, para configurar o gerador de código aleatório(rand), baseado no time.
-    unsigned int codigo = rand(); //Salvar o código usando rand, com as configurações do srand.
-    cout << "\nO código é: " << codigo << endl;
+    unsigned int codigo;
+    codigo = rand(); //Salvar o código usando rand, com as configurações do srand.
     return codigo;
 }
 
 string salvarNome() //Função para pegar o nome.
 {
     string nome;
-    cout << "Qual é o nome? ";
+    cout << "Digite o nome? ";
     getline(cin >> ws,nome); //Capturar string/linha(nome) do funcionário.
     return nome;
 }
 
 string salvarTelefone() //Função para pegar o telefone do funcionário.
 {
+    int verificador=0, x, i;
     string telefone;
     cout << "Digite o telefone: ";
-    getline(cin >> ws,telefone);
+    do
+    {
+        getline(cin >> ws,telefone);
+        for(i=0; i<telefone.length(); i++)
+        {
+            x=isdigit(telefone[i])?1:2; //1 para numerais, 2 para caracteres.
+            if(x==1)
+            {
+                verificador++; //Se for numeral, incrementa.
+            }
+        }
+        if(verificador<10)
+        {
+            cout << "Número de telefone inválido! [Mínimo de 10 números (DDD + número)]" << endl;
+            cout << "Digite um telefone válido: ";
+        }
+    } while (verificador<10); //Se tiver no mínimo 10 numerais, conclui.
     return telefone;
 }
 
@@ -94,25 +116,46 @@ float salvarSalario() //Função para pegar o salário do funcionário.
 {
     float salario;
     cout << "Digite o salário: ";
-    cin >> salario;
+    do
+    {
+        scanf("%f", &salario);
+        if(salario < 0)
+        {
+            cout << "Salário inválido!\nDigite um salário positivo: ";
+
+        }
+    } while (salario < 0);
     return salario;
 }
 
-char salvarTipo() //Função para pegar o tipo do funcionário.
+string salvarTipo() //Função para pegar o tipo do funcionário.
 {
-    char tipo;
-    cout << "\nMenu de opções:";
+    char opcao;
+    string tipo;
+    cout << "Menu de opções:";
     cout << "\nT - Para temporário\nF - Para fixo";
-    cout << "\nQual o tipo do funcionário? ";
+    cout << "\nDigite o tipo do funcionário? ";
     do
     {
-        scanf(" %c", &tipo);
-        tipo = toupper(tipo);
-        cout << tipo << endl;
-        if(tipo!='T' && tipo!='F')
+        scanf(" %c", &opcao);
+        opcao = toupper(opcao);
+        if(opcao!='T' && opcao!='F')
         {
             cout << "Informe uma opção válida(T ou F): ";
         }
-    } while (tipo!='T' && tipo!='F');
+    } while (opcao!='T' && opcao!='F');
+
+    switch (opcao)
+    {
+        case 'T':
+            tipo = "Temporário";
+            break;
+        case 'F':
+            tipo = "Fixo";
+            break;
+        default:
+            cout << "Erro, por favor tente novamente.\n" << endl;
+            break;
+    }
     return tipo;
 }
