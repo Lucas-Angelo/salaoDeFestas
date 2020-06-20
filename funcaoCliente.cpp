1f20eff38573d8c6aac609f656969177c9ed4b99
Marco2202 <tulliomarco93@gmail.com>
2020-06-19 21:55:30 -0300

    Menu e Cliente
    -Mudancas no menu.
    -Codigo cliente valida telefone.

--- funcaoCliente.cpp
+++ funcaoCliente.cpp
@@ -1,35 +1,54 @@
 #include <iostream>
 #include <stdio.h>
 #include "munit.h"
+#include <locale.h>
+#include <ctype.h>
 
 using namespace std;
-#include <string>
+#include <string.h>
 #include <time.h>
 #include "ClienteClass.h"
 #include "helpers/ModelHelper.h"
 #include <fstream>
+#include "helpers/DateHelper.h"
 
-
+string salvarData();
+unsigned int gerarCodigos();
+unsigned long salvarTelefones();// unsigned so pega valor positivo
 int funcaoCliente()
 {
+
     ClienteClass cli;
     setlocale(LC_ALL,"portuguese"); //colocar acento
     srand((unsigned int) time(NULL)); //configura a funcao aleatoria
+    char c='a';
+    while(toupper(c)!='C')
+    {
+        /*cout << "\n------------>CADASTRO DE CLIENTE<------------" << endl;
+        cli.codigo = gerarCodigos();
 
-    printf("\n------------>CADASTRO DE CLIENTE<------------\n");
-    cli.codigo = rand();
+        cout << "\nDigite o nome: ";
+        getline(cin >> ws,cli.nome);
 
-    printf("\nDigite o nome: ");
-    getline(cin >> ws,cli.nome);
+        cout <<"Digite o endereço: ";
+        getline(cin >> ws,cli.endereco);//ws pega a linha inteira
 
-    printf("\nDigite o endereço: ");
-    getline(cin >> ws,cli.endereco);
+        cli.telefone = salvarTelefones();*/
+        cli.dtNascimento = salvarData();
 
-    printf("\nDigite o telefone: ");
-    cin>>cli.telefone;
+        /*cout << "\nDados do Cliente:" << endl;
+        printf("Código: %d\n", cli.codigo);
+        cout << "Nome: " + cli.nome << endl;
+        cout << "Telefone: " << cli.telefone << endl;*/
+        cout << "Data: " + cli.dtNascimento << endl;
 
-    printf("\nDigite a data de nascimento: ");
-    getline(cin >> ws,cli.dtNascimento);
+        printf("\n--->CONFIRMAR DADOS<---");
+        printf("\nC - Para confirmar");
+        printf("\nR - Para refazer");
+        printf("\nOs dados inseridos estão corretos? ");
+        cin >> c;
+    }
+    cli.save();
 
     return 0;
 }
@@ -64,6 +83,34 @@ void procuraCliente() {
         }
     }
 }
+unsigned int gerarCodigos()
+{
+    srand((unsigned int)time(NULL));
+    unsigned int codigo;
+    codigo = rand();
+    return codigo;
+}
+unsigned long salvarTelefones()
+{
+    unsigned int telefone;
+    cout << "Digite o telefone: ";
+    cin >> telefone;
+    while(telefone < 1000000000 || telefone > 9999999999 )
+    {
+    cout << "Digite um telefone valido: " ;
+    cin >> telefone;
+    }
+    return telefone;
+}
+string salvarData()
+{
+    DateHelper dh; //Chamar classe DataHelper, que verifica se a data inserida é válida
+    cout << "Digite a data(dd/mm/AAAA): ";
+    string data = dh.inputDate(); //Equivale ao cin da data, chamando a função para verificar se é válida
+    cout << "data: " << data;
+    return data;
+}
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -11,103 +11,91 @@ using namespace std;
 #include "FornecedorClass.h"
 #include "funcaoCliente.h"
 
+
 using namespace std;
 void printArt();
 
-int main()
-{
-    int op=0,cad,pes;
-    setlocale(LC_ALL,"portuguese");
-    printArt();
-    while(op!=3)
+int main() {
+  int op = 0, cad;
+  setlocale(LC_ALL, "portuguese");
+  printArt();
+  while (op != 3)
     {
-      printf("\n---------------->MENU DE OPCOES<----------------\n");
-      printf("\n1-Cadastrar\n2-Pesquisar\n3-Sair\nDigite uma opcao: ");
-      scanf("%d",&op);
-      cad=0;
-      pes=0;
-      if(op ==1)
+    cout << "\n---------------->MENU DE OPCOES<----------------" << endl;
+    cout << "\n1-Cadastrar\n2-Pesquisar\n3-Sair\nDigite uma opcao: ";
+    cin >> op;
+    cad = 0;
+    if (op == 1)
+    {
+      cout << "\n------------>OPCOES DE CADASTRO<------------" << endl;
+      cout << "\n1-Cliente\n2-Funcionario\n3-Fornecedor\n4-Festa\n5-Voltar\nDigite o que gostaria de cadastrar: ";
+      cin >> cad;
+      switch (cad)
       {
-        printf("\n------------>OPCOES DE CADASTRO<------------\n");
-        printf("\n1-Cliente\n2-Funcionario\n3-Fornecedor\n4-Festa\n5-Voltar\nDigite o que gostaria de cadastrar: ");
-        scanf("%d",&cad);
-      }
-      else if(op==2)
-      {
-        printf("\n------------>OPCOES DE PESQUISA<------------\n");
-        printf("\n1-Cliente\n2-Funcionario\n3-Fornecedor\n4-Voltar\nDigite o que gostaria de pesquisar: ");
-        scanf("%d",&pes);
-      }
+      case 1:
+        funcaoCliente();
+        break;
+      case 2:
+        funcaoFuncionarios(op, cad);
+        break;
+      case 3:
+        funcaoFornecedor();
+        break;
+      case 4:
+        //festa
+        break;
 
-      if(cad !=0)
-      {
-          switch(cad)
-          {
-          case 1:
-            funcaoCliente();
-          break;
-          case 2:
-            funcaoFuncionarios(op, cad);
-          break;
-          case 3:
-            funcaoFornecedor ();
-          break;
-           case 4:
-            //festa
-          break;
-
-          default:
-            cad=0;
-        }
+      default:
+        cad = 0;
       }
-      if(pes !=0)
+    } else if (op == 2)
+    {
+      cout << "\n------------>OPCOES DE PESQUISA<------------" << endl;
+      cout << "\n1-Cliente\n2-Funcionario\n3-Fornecedor\n4-Voltar\nDigite o que gostaria de pesquisar: ";
+      cin >> cad;
+      switch (cad)
       {
-          switch(pes)
-          {
-          case 1:
-             procuraCliente();
-          break;
-          case 2:
-           procuraFuncionario();
-          break;
-          case 3:
-           //procurar fornecedor
-           funcaoFornecedor ();
-          break;
-          case 4:
-            //procurar festa
-            procuraCliente();
-          break;
-          default:
-            pes=0;
-        }
+      case 1:
+        procuraCliente();
+        break;
+      case 2:
+        procuraFuncionario();
+        break;
+      case 3:
+        //procurar fornecedor
+        funcaoFornecedor();
+        break;
+      case 4:
+        //procurar festa
+        break;
+      default:
+        cad = 0;
       }
     }
-
+  }
     return 0;
 }
 
-void printArt(){
-    cout << "-----------------------------------------------------" << endl;
-    cout << "     Salao de Festas - Patati Patata       " << endl;
-    cout << "   _                             .-." << endl;
-    cout << "  / )  .-.    ___          __   (   )" << endl;
-    cout << " ( (  (   ) .'___)        (__'-._) (" << endl;
-    cout << "  \\ '._) (,'.'               '.     '-." << endl;
-    cout << "   '.      /  \"\\               '    -. '." << endl;
-    cout << "     )    /   \\ \\   .-.   ,'.   )  (  ',_)    _" << endl;
-    cout << "   .'    (     \\ \\ (   \ . .' .'    )    .-. ( \ " << endl;
-    cout << "  (  .''. '.    \\ \\|  .' .' ,',--, /    (   ) ) )" << endl;
-    cout << "   \\ \\   ', :    \\    .-'  ( (  ( (     _) (,' / " << endl;
-    cout << "    \\ \\   : :    )  / _     ' .  \ \  ,'      / " << endl;
-    cout << "  ,' ,'   : ;   /  /,' '.   /.'  / / ( (\    (" << endl;
-    cout << "  '.'      \"   (    .-'. \       ''   \_)\    \ " << endl;
-    cout << "                \\  |    \ \__             )    )" << endl;
-    cout << "              ___\\ |     \___;           /  , /" << endl;
-    cout << "             /  ___)                    (  ( (" << endl;
-    cout << "             '.'                         ) ;) ;" << endl;
-    cout << "                                        (_/(_/" << endl;
-    cout << "----------------------------------------------------" << endl;
-
+void printArt() {
+  cout << "-----------------------------------------------------" << endl;
+  cout << "     Salao de Festas - Patati Patata       " << endl;
+  cout << "   _                             .-." << endl;
+  cout << "  / )  .-.    ___          __   (   )" << endl;
+  cout << " ( (  (   ) .'___)        (__'-._) (" << endl;
+  cout << "  \\ '._) (,'.'               '.     '-." << endl;
+  cout << "   '.      /  \"\\               '    -. '." << endl;
+  cout << "     )    /   \\ \\   .-.   ,'.   )  (  ',_)    _" << endl;
+  cout << "   .'    (     \\ \\ (   \ . .' .'    )    .-. ( \ " << endl;
+  cout << "  (  .''. '.    \\ \\|  .' .' ,',--, /    (   ) ) )" << endl;
+  cout << "   \\ \\   ', :    \\    .-'  ( (  ( (     _) (,' / " << endl;
+  cout << "    \\ \\   : :    )  / _     ' .  \ \  ,'      / " << endl;
+  cout << "  ,' ,'   : ;   /  /,' '.   /.'  / / ( (\    (" << endl;
+  cout << "  '.'      \"   (    .-'. \       ''   \_)\    \ " << endl;
+  cout << "                \\  |    \ \__             )    )" << endl;
+  cout << "              ___\\ |     \___;           /  , /" << endl;
+  cout << "             /  ___)                    (  ( (" << endl;
+  cout << "             '.'                         ) ;) ;" << endl;
+  cout << "                                        (_/(_/" << endl;
+  cout << "----------------------------------------------------" << endl;
 
 }
